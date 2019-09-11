f3ce545b2c7156c605e9ae7ba56d60195e44a45d
aluno <aluno@myco.lxd>
2019-09-11 19:51:58 +0000

    11-09

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
@@ -6,11 +6,19 @@ app=Flask(__name__)
 def index():
 	 return "Olá"
 @app.route("/pagina")
-def index():
+def pagina():
 	 return render_template("index.html")
 
 @app.route("/test")
 def test():
 	 return "<input type='submit' value='intervalo'>"
+
+@app.route("/nmap")
+def nmap():
+	import subprocess
+	proc = subprocess.Popen(['nmap','-sP','10.120.71.*'],
+		stdout=subprocess.PIPE)
+	ret=proc.stdout.read()
+	return ret
 
 app.run(debug=True,host="0.0.0.0")
